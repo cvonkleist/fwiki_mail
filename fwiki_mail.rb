ab301ef9e0e7a5f850530c462daeb78f8faeb92e
Christian von Kleist <cvonkleist@gmail.com>
2010-12-22 18:25:58 -0500

    has moar commintz

--- fwiki_mail.rb
+++ fwiki_mail.rb
@@ -1,11 +1,16 @@
 require 'fwiki_api'
 
 module FwikiMail
+  # the parser turns an e-mail message into Edit objects
   class Parser
     def initialize(email)
       @email = email
     end
 
+    # returns the e-mail minus headers (including the "Subject: " part of the
+    # subject header)
+    #
+    # the subject header can contain part of the message (e.g., ">foo")
     def message
       if subject && !subject.empty?
         subject + "\n" + body
@@ -14,10 +19,12 @@ module FwikiMail
       end
     end
 
+    # returns all the edit segments in the message as an array
     def segments
       message.split(%r(\n\n(?=[<>])))
     end
 
+    # returns the segments as Edit objects
     def edits
       segments.collect do |segment|
         Edit.new(segment)
@@ -34,11 +41,15 @@ module FwikiMail
     end
   end
 
+  # an edit is an order to change a wiki page
   class Edit
+    # +segment+ is a section of a message (e.g, ">foo\nbar\n")
     def initialize(segment)
       @segment = segment
     end
 
+    # interprets the edit-operation characters and returns a symbol
+    # representing the type of edit
     def operation
       case @segment[%r(^([<>]{1,2})), 1]
       when '>': :replace
@@ -48,28 +59,32 @@ module FwikiMail
       end
     end
 
+    # title of the page the edit should act on
     def title
       @segment[%r(^[<>]{1,2}(.*)$), 1]
     end
 
+    # new text to apply to the page
     def body
       @segment[%r(^.*?\n(.*)$)m, 1]
     end
 
-    def inspect
-      '#<Edit: operation = %s / title = %s / body = %s>' % [operation.inspect, title.inspect, body.inspect]
-    end
-
+    # the segment originally parsed by #initialize
     def to_s
       @segment
     end
   end
 
+  # given an e-mail message and a fwiki connection, apply all the edits in the
+  # message to the wiki
   class Processor
     def initialize(email, connection)
       @email, @connection = email, connection
     end
 
+    # perform each edit, collecting response messages
+    #
+    # returns an array of edit responses (summaries of success or failure)
     def run
       edits.collect do |edit|
         begin
@@ -80,6 +95,9 @@ module FwikiMail
       end
     end
 
+    # perform the specified +edit+
+    #
+    # returns an edit response (summary of the successful edit)
     def process(edit)
       body = @connection.read(edit.title) rescue nil
 
@@ -111,6 +129,8 @@ module FwikiMail
     end
   end
 
+  # an edit response is a message like 'success: appended to "foobar"', but as
+  # an object
   class EditResponse
     def initialize(status, message)
       @status, @message = status, message
@@ -120,17 +140,23 @@ module FwikiMail
     end
   end
 
+  # given login parameters details and an e-mail message, Runner connects to a
+  # fwiki instance and makes a new Processor to perform the edits
   class Runner
     def initialize(host, port, username, password, email)
       @connection = FwikiAPI::Connection.new(host, port, username, password)
       @email = email
     end
 
+    # launch a processor to perform the edits specified in +@email+
+    #
+    # returns EditResponse objects as an array
     def run!
       processor = Processor.new(@email, @connection)
       @edit_responses = processor.run
     end
 
+    # turns the collected +@edit_reponses+ into a printable message
     def report
       @edit_responses.collect { |er| '- ' + er.to_s }.join("\n")
     end
@@ -142,11 +168,15 @@ if $0 == __FILE__
     puts 'usage: %s host port username password < email.txt' % File.basename($0)
     exit 1
   end
+
   host, port, username, password, report_email_address = ARGV
   email = STDIN.read
+
   runner = FwikiMail::Runner.new(host, port, username, password, email)
   runner.run!
+
   puts runner.report
+
   if report_email_address
     IO.popen('mail ' + report_email_address, 'w') do |mail|
       mail.puts runner.report
